860a3276a2f62f7497ed155284200ff919ec09a1
PannnTastic <pannntastic@gmail.com>
2025-12-26 21:00:19 +0700

    Refactor: Change type of Siswa ID from String to Int in repository layer

--- app/src/main/java/com/example/aplikasimysql/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/aplikasimysql/repositori/RepositoriDataSiswa.kt
@@ -8,10 +8,10 @@ interface RepositoriDataSiswa {
     suspend fun getSiswa(): List<DataSiswa>
     suspend fun insertSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
 
-    suspend fun getSatuSiswa(id: String): DataSiswa
+    suspend fun getSatuSiswa(id: Int): DataSiswa
 
-    suspend fun updateSiswa(id: String, dataSiswa: DataSiswa): retrofit2.Response<Void>
-    suspend fun deleteSiswa(id: String): retrofit2.Response<Void>
+    suspend fun updateSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun deleteSiswa(id: Int): retrofit2.Response<Void>
 
 }
 
@@ -22,7 +22,7 @@ class JaringanRepositoriDataSiswa(
     override suspend fun insertSiswa(dataSiswa: DataSiswa)
     :retrofit2.Response<Void> = serviceApiSiswa.insertSiswa(dataSiswa)
 
-    override suspend fun getSatuSiswa(id: String): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
-    override suspend fun updateSiswa(id: String,dataSiswa: DataSiswa): Response<Void>  = serviceApiSiswa.updateSiswa(id, dataSiswa)
-    override suspend fun deleteSiswa(id: String): Response<Void>  = serviceApiSiswa.deleteSiswa(id)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun updateSiswa(id: Int,dataSiswa: DataSiswa): Response<Void>  = serviceApiSiswa.updateSiswa(id, dataSiswa)
+    override suspend fun deleteSiswa(id: Int): Response<Void>  = serviceApiSiswa.deleteSiswa(id)
 }
