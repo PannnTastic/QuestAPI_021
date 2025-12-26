b76542e8ad4d15a2b51d30d56512da3e066b357a
PannnTastic <pannntastic@gmail.com>
2025-12-26 20:29:11 +0700

    Add ViewModel initializers for DetailViewModel and EditViewModel

--- app/src/main/java/com/example/aplikasimysql/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/aplikasimysql/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.aplikasimysql.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.aplikasimysql.repositori.AplikasiDataSiswa
+import com.example.aplikasimysql.viewmodel.DetailViewModel
+import com.example.aplikasimysql.viewmodel.EditViewModel
 import com.example.aplikasimysql.viewmodel.EntryViewModel
 import com.example.aplikasimysql.viewmodel.HomeViewModel
 
@@ -19,6 +22,12 @@ object PenyediaViewModel{
         }
         initializer {
             EntryViewModel(aplikasiDataSiswa().container.repositoriDataSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositoriDataSiswa)
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositoriDataSiswa)
         }
     }
 }
