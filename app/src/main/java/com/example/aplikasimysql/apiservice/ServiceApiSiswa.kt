ad20fa65c680a90cb256613fd0b3aa277811b4d5
PannnTastic <pannntastic@gmail.com>
2025-12-26 20:02:24 +0700

    Add CRUD operations to ServiceApiSiswa for managing student data

--- app/src/main/java/com/example/aplikasimysql/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/aplikasimysql/apiservice/ServiceApiSiswa.kt
@@ -1,9 +1,13 @@
 package com.example.aplikasimysql.apiservice
 
 import com.example.aplikasimysql.modeldata.DataSiswa
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ServiceApiSiswa {
     @GET("teman")
@@ -11,4 +15,13 @@ interface ServiceApiSiswa {
 
     @POST("teman")
     suspend fun insertSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @GET("teman/{id}")
+    suspend fun getSatuSiswa(@Path("id") id: String): DataSiswa
+
+    @PUT("teman/{id}")
+    suspend fun updateSiswa(@Path("id") id: String, @Body dataSiswa: DataSiswa): Response<Void>
+
+    @DELETE("teman/{id}")
+    suspend fun deleteSiswa(@Path("id") id: String): Response<Void>
 }
