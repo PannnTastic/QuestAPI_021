3d11a74d6c3887570460844ebeaa6886e4a4b7b8
PannnTastic <pannntastic@gmail.com>
2025-12-26 20:06:16 +0700

    Add methods for retrieving, updating, and deleting student data in RepositoriDataSiswa

--- app/src/main/java/com/example/aplikasimysql/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/aplikasimysql/repositori/RepositoriDataSiswa.kt
@@ -2,10 +2,17 @@ package com.example.aplikasimysql.repositori
 
 import com.example.aplikasimysql.apiservice.ServiceApiSiswa
 import com.example.aplikasimysql.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoriDataSiswa {
     suspend fun getSiswa(): List<DataSiswa>
     suspend fun insertSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    suspend fun getSatuSiswa(id: String): DataSiswa
+
+    suspend fun updateSiswa(id: String, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun deleteSiswa(id: String): retrofit2.Response<Void>
+
 }
 
 class JaringanRepositoriDataSiswa(
@@ -14,4 +21,8 @@ class JaringanRepositoriDataSiswa(
     override suspend fun getSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun insertSiswa(dataSiswa: DataSiswa)
     :retrofit2.Response<Void> = serviceApiSiswa.insertSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(id: String): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun updateSiswa(id: String,dataSiswa: DataSiswa): Response<Void>  = serviceApiSiswa.updateSiswa(id, dataSiswa)
+    override suspend fun deleteSiswa(id: String): Response<Void>  = serviceApiSiswa.deleteSiswa(id)
 }
